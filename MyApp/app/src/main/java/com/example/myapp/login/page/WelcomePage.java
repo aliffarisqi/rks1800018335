f2c2ffa468b1161db691c03d082ff25ac941a8af
bayualiffarisqi <jakartabanyu@gmail.com>
2020-12-14 23:06:30 +0700

    add assets in drawable

--- MyApp/app/src/main/java/com/example/myapp/login/page/WelcomePage.java
+++ MyApp/app/src/main/java/com/example/myapp/login/page/WelcomePage.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.myapp.R;
+
 
 public class WelcomePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcomePage);
     }
 }

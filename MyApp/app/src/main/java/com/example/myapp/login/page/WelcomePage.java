9c3b696418e46258da94e4986acbcfb29fce106e
bayualiffarisqi <jakartabanyu@gmail.com>
2020-12-14 22:11:22 +0700

    create login page and ekspore assets

---
+++ MyApp/app/src/main/java/com/example/myapp/login/page/WelcomePage.java
@@ -0,0 +1,13 @@
+package com.example.myapp.login.page;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class WelcomePage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+}

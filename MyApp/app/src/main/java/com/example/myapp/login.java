9ab80b6aa1f1c3ec2458a585441ec5b55dd9feef
bayualiffarisqi <jakartabanyu@gmail.com>
2020-12-19 23:51:58 +0700

    add button click function in login

--- MyApp/app/src/main/java/com/example/myapp/login.java
+++ MyApp/app/src/main/java/com/example/myapp/login.java
@@ -2,13 +2,25 @@ package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
-
+    Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn1 = (Button) findViewById(R.id.next1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent inte = new Intent(login.this, Slides.class);
+                startActivity(inte);
+            }
+        });
     }
 }

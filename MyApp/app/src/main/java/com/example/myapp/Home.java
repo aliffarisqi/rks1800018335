a02e6c1be7b79ed9a3de1736bee3065c9f622722
bayualiffarisqi <jakartabanyu@gmail.com>
2021-01-01 14:02:54 +0700

    create home page activity and Home layout

--- MyApp/app/src/main/java/com/example/myapp/Home.java
+++ MyApp/app/src/main/java/com/example/myapp/Home.java
@@ -2,18 +2,13 @@ package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class login extends AppCompatActivity {
-    Button btn1;
+public class Home extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-
+        setContentView(R.layout.activity_home);
     }
 }

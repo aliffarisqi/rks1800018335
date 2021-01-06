86b5de5a3f9bf73bd7751e12fb8860dc15fcaf90
bayualiffarisqi <jakartabanyu@gmail.com>
2021-01-06 23:42:14 +0700

    add class menu

--- MyApp/app/src/main/java/com/example/myapp/Home.java
+++ MyApp/app/src/main/java/com/example/myapp/Home.java
@@ -2,14 +2,28 @@ package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.example.myapp.kelas.KelasHome;
 
 public class Home extends AppCompatActivity {
-
+    RelativeLayout btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        
+
+        btn1 = findViewById(R.id.layoutHome2);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent inte = new Intent(Home.this, KelasHome.class);
+                startActivity(inte);
+            }
+        });
     }
 }

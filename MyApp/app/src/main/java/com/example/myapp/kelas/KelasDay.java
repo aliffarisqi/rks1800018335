38aba6286518099b5293e30c6b05fc7298a6696b
bayualiffarisqi <jakartabanyu@gmail.com>
2021-01-07 23:43:00 +0700

    add day actifity in java and layout

---
+++ MyApp/app/src/main/java/com/example/myapp/kelas/KelasDay.java
@@ -0,0 +1,16 @@
+package com.example.myapp.kelas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapp.R;
+
+public class KelasDay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kelas_day);
+    }
+}
--- MyApp/app/src/main/java/com/example/myapp/kelas/KelasHome.java
+++ MyApp/app/src/main/java/com/example/myapp/kelas/KelasHome.java
@@ -2,15 +2,27 @@ package com.example.myapp.kelas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
+import com.example.myapp.Home;
 import com.example.myapp.R;
 
 public class KelasHome extends AppCompatActivity {
-
+    RelativeLayout btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kelas_home);
+        btn1 = findViewById(R.id.rl_senin);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent inte = new Intent(KelasHome.this, KelasDay.class);
+                startActivity(inte);
+            }
+        });
     }
 }
